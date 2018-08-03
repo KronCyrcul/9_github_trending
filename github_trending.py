d60ddb6a708a3dc6035e63119ee78f248a9deeae
KronCyrcul <ibloko1504@gmail.com>
2018-08-03 21:43:08 +0300

    delete get_open_issues_urls, get_open_issues_amount fucntions, add get_trending_repositories_info function, function get_open_issues_list called in it

--- github_trending.py
+++ github_trending.py
@@ -3,11 +3,10 @@ import requests
 import datetime
 
 
-def get_trending_repositories(top_size):
+def get_trending_repositories(top_size, days_ago):
     trending_repositories = []
     request_url = "https://api.github.com/search/repositories"
     today = datetime.datetime.today()
-    days_ago = 7
     past_date = today - datetime.timedelta(days=days_ago)
     search_params = {"q": " created:>{}".format(
         past_date.strftime("%Y-%m-%d")),
@@ -19,16 +18,19 @@ def get_trending_repositories(top_size):
     return trending_repositories
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    open_issues_list = get_open_issues_list(repo_owner, repo_name)
-    return len(open_issues_list)
-
-
-def get_open_issues_urls(repo_owner, repo_name):
-    open_issues_list = get_open_issues_list(repo_owner, repo_name)
-    open_issues_urls = [issue["url"] for issue in open_issues_list
-        if len(open_issues_list) > 0]
-    return open_issues_urls
+def get_trending_repositories_info(trending_repositories):
+    trending_repositories_info = []
+    for repo in trending_repositories:
+        repo_owner = repo['owner']['login']
+        repo_name = repo['name']
+        star_count = repo['stargazers_count']
+        open_issues_list = get_open_issues_list(repo_owner, repo_name)
+        open_issues_amount = len(open_issues_list)
+        open_issues_urls = [issue["url"] for issue in open_issues_list
+            if len(open_issues_list) > 0]
+        trending_repositories_info.append([repo_owner, repo_name, star_count,
+            open_issues_amount, open_issues_urls])
+    return (trending_repositories_info)
 
 
 def get_open_issues_list(repo_owner, repo_name):
@@ -39,14 +41,14 @@ def get_open_issues_list(repo_owner, repo_name):
     return open_issues_list
 
 
-def print_trending_repositories(trending_repositories):
+def print_trending_repositories(trending_repositories_info):
     print("Топ 20 репозиториев:")
-    for repo in trending_repositories:
-        repo_owner = repo['owner']['login']
-        repo_name = repo['name']
-        star_count = repo['stargazers_count']
-        open_issues_amount = get_open_issues_amount(repo_owner, repo_name)
-        open_issues_urls = get_open_issues_urls(repo_owner, repo_name)
+    for repo in trending_repositories_info:
+        repo_owner = repo[0]
+        repo_name = repo[1]
+        star_count = repo[2]
+        open_issues_amount = repo[3]
+        open_issues_urls = repo[4]
         print("{} / {}".format(repo_owner, repo_name))
         print("Stars: {}".format(star_count))
         if open_issues_amount > 0:
@@ -56,5 +58,8 @@ def print_trending_repositories(trending_repositories):
 
 if __name__ == "__main__":
     top_size = 20
-    trending_repositories = get_trending_repositories(top_size)
-    print_trending_repositories(trending_repositories)
+    days_ago = 7
+    trending_repositories = get_trending_repositories(top_size, days_ago)
+    trending_repositories_info = get_trending_repositories_info(
+        trending_repositories)
+    print_trending_repositories(trending_repositories_info)
